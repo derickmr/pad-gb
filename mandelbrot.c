f74f4d6392fc52d709cb43c9592d4c48c8cefb40
Derick Rentz <derick.rentz@sap.com>
2020-06-15 22:33:11 -0300

    Trying to fix stuff

--- mandelbrot.c
+++ mandelbrot.c
@@ -147,6 +147,10 @@ int main(int argc, char* argv[])
   int xres = atoi(argv[6]);
   int yres = (xres*(ymax-ymin))/(xmax-xmin);
     
+    if (yres%numThreads != 0){
+        yres -= yres%numThreads;
+    }
+    
     printf ("yres: %d \n", yres);
     
   int arraySize = yres * xres * COLOR_SIZE;
